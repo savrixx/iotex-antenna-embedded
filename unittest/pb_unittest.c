1b5147302df1fed9e83aa8af4a4690de73eb8fd1
amaork@Ubuntu16.04 <amaork@gmail.com>
2020-02-11 20:51:13 +0800

    add decode pb varint unittest code

--- unittest/pb_unittest.c
+++ unittest/pb_unittest.c
@@ -1,9 +1,71 @@
 #include <stdio.h>
+#include <inttypes.h>
 #include "unittest.h"
 #include "test_config.h"
 #include "../src/debug.h"
 #include "../src/signer.h"
 #include "../src/pb_pack.h"
+
+
+uint64_t
+decode_varint(const uint8_t *buf, uint8_t *skip_bytes, uint8_t max_len) {
+    uint64_t result = 0;
+    uint64_t val;
+    uint8_t idx = 0;
+
+    while (idx < max_len)
+    {
+        val = buf[idx] & 0x7f;
+        result |= (val << (idx * 7));
+
+        // no more bytes
+        if (!(buf[idx] & 0x80)) break;
+
+        idx++;
+    }
+
+    (*skip_bytes) = idx + 1;
+    return result;
+}
+
+
+char *u642str(uint64_t num, char *str, size_t max_len) {
+
+    char temp;
+    int last = 0;
+    char *start = str, *end = str;
+
+    if (0 == num) {
+        str[0] = '0';
+        str[1] = 0;
+        return str;
+    }
+
+    while (num != 0) {
+        if (end - start < max_len - 1) {
+            last = num % 10;
+            *end = last + '0';
+            num /= 10;
+            end++;
+        }
+        else {
+            /* buffer too short */
+            return NULL;
+        }
+    }
+
+    /* string ends with \0 */
+    *end = 0;
+
+    while (start < --end) {
+        temp = *start;
+        *start = *end;
+        *end = temp;
+        start++;
+    }
+
+    return str;
+}
 
 
 void test_pack(const char *test_name, pb_em_wtype test_type,
@@ -51,9 +113,11 @@ void test_pack(const char *test_name, pb_em_wtype test_type,
     UNITTEST_PASS(test_name);
 }
 
+
 void test_pack_varint() {
 
-    uint64_t datas[] = {1, 300, 0x123, 0x1f2f32, 0x13232323, 0x3214325412};
+    char key_value_str[40];
+    uint64_t datas[] = {1, 300, 0x123, 0x1f2f32, 0x13232323, 0x3214325412, 0x12345678ABCD, 0xABCDEF123456789};
     const char *serialize_str[] = {
 
         "0801",
@@ -62,9 +126,37 @@ void test_pack_varint() {
         "08b2de7c",
         "08a3c68c9901",
         "0892a8c9a1a106",
+        "08cdd7e2b3c5c604",
+        "0889cf959a92deb7de0a"
     };
 
-    test_pack(__func__, PB_WT_VARINT, datas, serialize_str, UNITTEST_GET_ARRAY_SIZE(datas), "080110ac0218a30220b2de7c28a3c68c99013092a8c9a1a106");
+    test_pack(__func__, PB_WT_VARINT, datas, serialize_str, UNITTEST_GET_ARRAY_SIZE(datas), "080110ac0218a30220b2de7c28a3c68c99013092a8c9a1a10638cdd7e2b3c5c6044089cf959a92deb7de0a");
+
+    /* Decode test */
+    size_t i;
+    int idx = 0;
+    uint64_t decode_datas[10];
+    uint8_t decode_buffer[256];
+    uint8_t write_type, key, value_len;
+    int decode_buffer_len = signer_str2hex("080110ac0218a30220b2de7c28a3c68c99013092a8c9a1a10638cdd7e2b3c5c6044089cf959a92deb7de0a", decode_buffer, sizeof(decode_buffer));
+
+    for (i = 0, idx = 0; i < UNITTEST_GET_ARRAY_SIZE(datas) && idx < decode_buffer_len; i++) {
+
+        key = PB_GET_FIELD(decode_buffer[idx]);
+        write_type = PB_GET_WTYPE(decode_buffer[idx]);
+
+        UNITTEST_ASSERT_EQ(key, i + 1);
+        UNITTEST_ASSERT_EQ(write_type, PB_WT_VARINT);
+        idx++;
+
+
+        decode_datas[i] = decode_varint(decode_buffer + idx, &value_len, decode_buffer_len - idx);
+
+        idx += value_len;
+
+        UNITTEST_ASSERT_EQ(datas[i], decode_datas[i]);
+        fprintf(stdout, "%lu: 0x%016" PRIx64", 0x%016" PRIx64", %s\n", i, datas[i], decode_datas[i], u642str(decode_datas[i], key_value_str, sizeof(key_value_str)));
+    }
 }
 
 
